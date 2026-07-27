c88c92daca5f11f5e203b09d36c8e785547f8887
Azimjon Yulchiev <yulchiev2211@mail.ru>
2026-07-27 12:10:42 +0500

    Add API declaration file for TypeScript

---
+++ src/api.d.ts
@@ -0,0 +1,9 @@
+export function fetchEquipment(): Promise<any[]>
+export function fetchEquipmentBySlug(slug: string): Promise<any>
+export function saveEquipment(item: any): Promise<any>
+export function deleteEquipmentApi(slug: string): Promise<any>
+export function fetchPartners(): Promise<any[]>
+export function savePartner(partner: any): Promise<any>
+export function updatePartner(id: number, partner: any): Promise<any>
+export function deletePartnerApi(id: number): Promise<any>
+export function seedDatabase(data: any[]): Promise<any>
